579e0c8c5137f06675b68aed77a5098e133af085
ashutoshkaushal95 <ashutoshkaushal2@gmail.com>
2022-10-19 12:47:22 +0530

    implemented react router and switch

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Banner from "./Banner";
+import Banner from "../Banner";
 import "./HomeScreen.css";
-import Nav from "./Nav";
-import requests from "./Requests";
-import Row from "./Row";
+import Nav from "../Nav";
+import requests from "../Requests";
+import Row from "../Row";
 
 function HomeScreen() {
     return <div className="homeScreen">
@@ -16,13 +16,10 @@ function HomeScreen() {
             fetchUrl={requests.fetchNetflixOriginals}
             isLargeRow
         />
-        {/* <Row title="Now Playing" fetchUrl={requests.fetchNowPlaying} /> */}
         <Row title="Trending on Netflix" fetchUrl={requests.fetchTrending} />
         <Row title="Top Rated of all Time" fetchUrl={requests.fetchTopRated} />
         <Row title="Popular on Netflix" fetchUrl={requests.fetchPopular} />
         <Row title="Popular TV Shows" fetchUrl={requests.fetchTvPopular} />
-        {/* <Row title="Latest" fetchUrl={requests.fetchLatest} />
-        <Row title="Upcoming" fetchUrl={requests.fetchUpcoming} /> */}
     </div>;
 }
 
