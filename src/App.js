29c21a2d7aeb4cd8b74b89df2eff7f4cc5178781
ashutoshkaushal95 <ashutoshkaushal2@gmail.com>
2022-10-21 18:12:23 +0530

    Loginscreen design complete

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import './App.css';
-import HomeScreen from './screens/HomeScreen';
+import React from "react";
+import "./App.css";
+import HomeScreen from "./screens/HomeScreen";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
 
 function App() {
+  const user = null;
+
+  //if there is defined user it will go to homescreen
+
   return <div className="app">
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <HomeScreen />
-        </Route>
-      </Switch>
+      {!user ? (
+        <LoginScreen />
+      ) : (
+        <Switch>
+          <Route exact path="/">
+            <HomeScreen />
+          </Route>
+        </Switch>
+      )}
     </Router>
   </div>;
 }
